3d1a248a52d38a6944b83b54d33c0333e44b729a
ranhomri <ran.homri@tensorleap.ai>
2023-12-05 12:41:43 +0200

    dependencies

--- cifar_example/config.py
+++ cifar_example/config.py
@@ -10,6 +10,20 @@ def load_od_config() -> Dict[str, Any]:
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
 
+    id_to_name = {
+        0: "airplane",
+        1: "automobile",
+        2: "bird",
+        3: "cat",
+        4: "deer",
+        5: "dog",
+        6: "frog",
+        7: "horse",
+        8: "ship",
+        9: "truck"
+    }
+    config['id_to_name'] = id_to_name
+
     return config
 
 
